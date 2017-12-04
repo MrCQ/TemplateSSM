a4316321b112f9a6b7d7e27397e2d16456885d71
Changqi <changqi.win@foxmail.com>
2017-12-04 10:52:42 +0800

    SSM脚手架，底层结构复用且自动生成

---
+++ template-api/src/main/java/com/template/api/GoodService.java
@@ -0,0 +1,13 @@
+package com.template.rpc.api;
+
+import com.zheng.common.base.BaseService;
+import com.template.dao.model.Good;
+import com.template.dao.model.GoodExample;
+
+/**
+* GoodService接口
+* Created by shuzheng on 2017/12/4.
+*/
+public interface GoodService extends BaseService<Good, GoodExample> {
+
+}
---
+++ template-api/src/main/java/com/template/api/OrderService.java
@@ -0,0 +1,13 @@
+package com.template.rpc.api;
+
+import com.zheng.common.base.BaseService;
+import com.template.dao.model.Order;
+import com.template.dao.model.OrderExample;
+
+/**
+* OrderService接口
+* Created by shuzheng on 2017/12/4.
+*/
+public interface OrderService extends BaseService<Order, OrderExample> {
+
+}
